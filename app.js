2795d7ebc499139d4c2c7d89ab195649dc820386
Bognar Junior <bognar_junior@yahoo.com.br>
2018-02-22 16:42:23 -0300

    Fifith Commit
    Quinto commit, criação de rota para post de produto

--- app.js
+++ app.js
@@ -10,6 +10,15 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Produto = require('./app/models/produto');
+
+//Conexão do Mlab
+mongoose.connect('mongodb://testenode:abcd123@ds243768.mlab.com:43768/node-crud-api', {
+    useMongoClient: true
+});
+//Localhost
+//mongoose.connect('mongodb://localhost/node-crud-api');  
 
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
@@ -18,9 +27,31 @@ const port = process.env.port || 8000;
 
 const router = express.Router();
 
+router.use(function (req, res, next) {
+    console.log('Algo está acontecendo aqui');
+    next();
+});
+
 router.get('/', function(req, res) {
     res.json({message: "Bem-vindo!"});
 });
+
+router.route('/produtos')
+    .post(function (req, res) { 
+        const produto = new Produto();
+
+        produto.nome = req.body.nome;
+        produto.preco = req.body.preco;
+        produto.descricao = req.body.descricao;
+        
+        produto.save(function (error) { 
+            if (error) {
+                res.send('Erro ao tentar salvar o produto: ' + error);
+            }
+
+            res.json({message: 'Produto cadastrado com sucesso!'});
+        });
+    })
 
 app.use('/api', router);
 
